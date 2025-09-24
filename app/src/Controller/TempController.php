73a83c5dfb7a5b9c7905244a05dc0dd59feaf8a4
Piotr Dul <piotr.firebrigade@gmail.com>
2025-09-24 11:51:18 +0200

    dodanie kontrolera przykladowego

---
+++ app/src/Controller/TempController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class TempController extends AbstractController
+{
+    #[Route('/temp', name: 'app_temp')]
+    public function index(): Response
+    {
+        return $this->render('temp/index.html.twig', [
+            'abc' => 'TempController works!',
+        ]);
+    }
+}
